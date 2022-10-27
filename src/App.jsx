fefda23e95ce7e4742351dd058ae243687999676
Chukwuebuka <azukachukwuebuka07@gmail.com>
2022-10-27 18:22:00 +0100

    social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Profile from './components/profile'
 import Links from './components/links'
+import github from './assets/github-svg.svg'
+import slack from './assets/slack-svg.svg'
 
 function App() {
 
@@ -11,6 +13,14 @@ function App() {
           <Links id="twitter" href="https://twitter.com/stilkinging">
             stilkinging
           </Links>
+      </div>
+      <div className="cs_my-2">
+        <div className="socials">
+            <img src={slack} alt="the logo of slack"/>
+          <a href="http://github.com/stilkinging">
+            <img src={github} alt="the logo of github "/>
+          </a>
+        </div>
       </div>
     </div>
   )
