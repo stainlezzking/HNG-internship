0404dd7ddf24964e58a6fdd2a8d0101ef5fc0afc
Chukwuebuka <azukachukwuebuka07@gmail.com>
2022-10-27 17:50:56 +0100

    profile section completed

---
+++ src/components/profile.jsx
@@ -0,0 +1,11 @@
+
+const Profile = () => (
+    <>
+        <img src="https://pbs.twimg.com/profile_images/1548077051499884547/SVzVAzA8_400x400.jpg" 
+        alt="Profile picture for this link_tree account" 
+        id="profile_img" />
+        <h3 className="display_name"> Chukwuebuka Stanley </h3>
+    </>
+)
+ 
+export default Profile;
