d0bab80831b81a81d693f586fee6314b33a69dc0
Chukwuebuka <azukachukwuebuka07@gmail.com>
2022-10-28 16:47:22 +0100

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 
+app.use(cors())
 app.use((req,res)=>res.json({ "slackUsername": 'stilkinging', "backend": true, "age": 22, "bio": "The greatest software developer in the making" }))
 
 const PORT = process.env.PORT || 5000
